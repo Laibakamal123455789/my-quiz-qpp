af1fc8e366fd8c9de5d848f69cc10fddb2960a81
Laibakamal123455789 <168301675+Laibakamal123455789@users.noreply.github.com>
2024-12-04 23:40:29 +0500

    quiz app

---
+++ app/data.js
@@ -0,0 +1,150 @@
+export const data = [
+    {
+      question: "Who is making the web standards?",
+        option1: "Mozilla",
+         option2: "Microsoft",
+         option3: "The World Wide Web Consortium",
+         option4: "Google",
+         ans:3,
+    },
+    {
+      question: "How can you open a link in a new browser window?",
+      
+         option1: "Target", 
+         option2: "_blank", 
+         option3: "Same", 
+         option4: "Open", 
+         ans:2
+    
+    },
+    {
+      question: "Which tag is used to define a hyperlink in HTML?",
+   
+           option1: "<a>", 
+           option2: "<link>", 
+           option3: "<href>", 
+           option4: "<url>", 
+          ans:1
+        
+    },
+    {
+      question: "Which attribute is used to specify the image source in HTML?",
+    
+           option1: "<a>",
+           option2: "<link>", 
+           option3: "<href>", 
+           option4: "<url>", 
+          ans:1
+        
+    },
+    {
+      question: "What does the <title> tag define in an HTML document?",
+     
+           option1: "The title displayed inside the document", 
+           option2: "The heading of the body", 
+           option3: "The title displayed in the browser tab", 
+           option4: "The background color of the page", 
+          ans:3
+      
+    },
+    {
+      question: "Which CSS property is used to add space between the text and its container?",
+    
+           option1: "border", 
+           option2: "margin", 
+           option3: "spacing", 
+          option4: "padding",
+          ans:4
+
+    },
+    {
+      question: "How do you select an element with the class name example in CSS?",
+      
+           option1: "#example", 
+           option2: ".example", 
+           option3: "spacing", 
+           option4: "padding", 
+          ans:2
+     
+    },
+    {
+      question: "What is the default display value for an HTML <div> element?",
+    
+           option1: "inline-block",
+           option2: "inline", 
+           option3: "block",
+           option4: "none", 
+          ans:3
+       
+    },
+    {
+      question: "How do you center an element horizontally in CSS?",
+   
+           option1: "text-align: center;",
+           option2: "align-items: center;",
+           option3: "justify-content: center;",
+          option4: "margin: 0 auto", 
+           ans:4
+     
+    },
+    {
+      question: "What is the default value of the position property in CSS?",
+     
+           option1: "absolute", 
+           option2: "static",
+           option3: "fixed", 
+           option4: "relative",
+          ans:2
+        
+    },
+    {
+      question: "Which JavaScript function is used to display an alert box with a message?",
+   
+          option1: "alert()",
+           option2: "msg()", 
+           option3: "log()",
+          option4: "print()",
+          ans:1
+       
+    },
+    {
+      question: "Which operator is used to compare both value and type in JavaScript",
+
+           option1: "==", 
+           option2: "!=", 
+           option3: "===", 
+           option4: "!==",
+          ans:3
+      
+    },
+    {
+      question: "Which of the following is the correct way to declare a JavaScript variable?",
+           option1: "let 10 = x", 
+           option2: "variable x = 10", 
+           option3: "int x = 10;", 
+           option4: "var x = 10;", 
+          ans:4
+    
+    },
+    {
+      question: "Which method is used to round a number to the nearest integer in JavaScript?",
+ 
+           option1: "Math.ceil()", 
+           option2: "Math.floor()", 
+           option3: "Math.random()", 
+          option4: "Math.round()", 
+          ans:4
+    
+    },
+    {
+      question: "Which of the following is used to define a function in JavaScript?",
+         option1: "function myFunction() { }",
+           option2: "def myFunction() { }", 
+           option3: "func myFunction() { }",
+           option4: "create function myFunction() { }",
+          ans:1
+    
+    }
+  
+  
+  ];
